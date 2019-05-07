c52bdd646376c0576b92e62752109ad23ee5440f
KanashiXI <panyapat.test@gmail.com>
2019-05-08 02:35:06 +0700

    insert data

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once '/lib/Slim/Slim/Slim.php';
-require_once '/controllers/TestController.php';
+require_once '/controllers/StudentController.php';
 
   Slim\Slim::registerAutoloader();
   $app = new Slim\Slim();
@@ -12,16 +12,26 @@ require_once '/controllers/TestController.php';
     echo json_encode($response, JSON_PRETTY_PRINT);
   }
 
-  $app->get('/', ['TestController', 'index']);
+  $app->get('/hello',['StudentController','index']);
+  $app->get('/findByName/:All',function($name){
+    StudentController::findByName($name);
+  });
+  $app->post('/search', function() use($app){
+    StudentController::search($app->request());
+  });
   $app->post('/insert', function() use($app){
-    TestController::insert($app->request());
+    StudentController::insert($app->request());
   });
-  $app->get('/search/:name', function ($name) {
-    TestController::search($name);
-  });
-  $app->get('/getdata/:age', function ($age) {
-    TestController::getdata($age);
-  });
+  // $app->get('/', ['TestController', 'index']);
+  // $app->post('/insert', function() use($app){
+  //   TestController::insert($app->request());
+  // });
+  // $app->get('/search/:name', function ($name) {
+  //   TestController::search($name);
+  // });
+  // $app->get('/getdata/:age', function ($age) {
+  //   TestController::getdata($age);
+  // });
 
   $app->run();
 ?>
